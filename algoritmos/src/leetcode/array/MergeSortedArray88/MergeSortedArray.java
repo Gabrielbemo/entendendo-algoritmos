9db467435e9aefc38ee5618480cba6f87e88ea46
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-17 19:41:54 -0300

    adding leetcode 88

---
+++ algoritmos/src/leetcode/array/MergeSortedArray88/MergeSortedArray.java
@@ -0,0 +1,32 @@
+package leetcode.array.MergeSortedArray88;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int firstWriteIndex = m + n - 1;
+        int lastValidIndexFirst = m - 1;
+        int lastValidIndexSecond = n - 1;
+
+        while (lastValidIndexSecond >= 0) {
+
+            if (lastValidIndexFirst >= 0 && nums1[lastValidIndexFirst] > nums2[lastValidIndexSecond]) {
+                nums1[firstWriteIndex] = nums1[lastValidIndexFirst];
+                lastValidIndexFirst--;
+            } else {
+                nums1[firstWriteIndex] = nums2[lastValidIndexSecond];
+                lastValidIndexSecond--;
+            }
+            firstWriteIndex--;
+        }
+    }
+}
