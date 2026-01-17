130e3c18aa8d29d730e107d14eb8e6e3b4d8285e
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-17 19:05:05 -0300

    adding leetcode 35

---
+++ algoritmos/src/leetcode/array/SearchInsertPosition35/README.md
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/search-insert-position?envType=problem-list-v2&envId=array
+
+## Comparação
+
+| Método        | Complexidade de Tempo | Complexidade de Espaço |
+|---------------|-----------------------|------------------------|
+| searchInsert  | O(log n)              | O(1)                   |
+
+
+Coding Pattern utilizado:
+
+searchInsert - Binary Search
---
+++ algoritmos/src/leetcode/array/SearchInsertPosition35/SearchInsertPosition.java
@@ -0,0 +1,30 @@
+package leetcode.array.SearchInsertPosition35;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 2;
+        System.out.println(searchInsert(nums, target));
+    }
+    public static int searchInsert(int[] nums, int target) {
+        int leftIndex = 0;
+        int rightIndex = nums.length - 1;
+
+        while (leftIndex <= rightIndex) {
+            int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+            int middleValue = nums[middleIndex];
+
+            if (middleValue == target) {
+                return middleIndex;
+            }
+
+            if (middleValue < target) {
+                leftIndex = middleIndex + 1;
+            } else {
+                rightIndex = middleIndex - 1;
+            }
+        }
+
+        return leftIndex;
+    }
+}
