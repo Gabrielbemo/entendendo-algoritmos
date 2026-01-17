b628031bacc3698af035a06e6f7cf9139772c552
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-17 11:56:53 -0300

    adding leetcode 26 category array

---
+++ algoritmos/src/leetcode/array/RemoveDuplicatesFromSortedArray26/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,22 @@
+package leetcode.array.RemoveDuplicatesFromSortedArray26;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] a = {0,0,1,1,1,2,2,3,3,4};
+        removeDuplicates(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int left = 0;
+        for(int right = 1; right < nums.length; right++){
+            if(nums[right] > (nums[left])){
+                left++;
+                nums[left] = nums[right];
+            }
+        }
+        return left+1;
+    }
+}
