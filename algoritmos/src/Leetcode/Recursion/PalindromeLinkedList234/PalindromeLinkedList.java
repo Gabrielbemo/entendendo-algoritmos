805f0d1f72103de625f88f3a108d80d9ceb48965
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-18 19:40:41 -0300

    adding leetcode 234

---
+++ algoritmos/src/Leetcode/Recursion/PalindromeLinkedList234/PalindromeLinkedList.java
@@ -0,0 +1,41 @@
+package Leetcode.Recursion.PalindromeLinkedList234;
+
+import Leetcode.Util.ListNode;
+
+public class PalindromeLinkedList {
+    static void main(String[] args) {
+        ListNode list1 = ListNode.createListNode(new int[]{1, 2, 3, 4, 2, 1});
+        ListNode.printList(list1);
+        System.out.println(isPalindrome(list1));
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        ListNode slowNode = head;
+        ListNode fastNode = head;
+        ListNode previousNode = null;
+
+        while (fastNode != null && fastNode.next != null) {
+            fastNode = fastNode.next.next;
+
+            ListNode nextNodeAfterMoving = slowNode.next;
+            slowNode.next = previousNode;
+            previousNode = slowNode;
+            slowNode = nextNodeAfterMoving;
+        }
+
+        if (fastNode != null) {
+            slowNode = slowNode.next;
+        }
+
+        while (slowNode != null && previousNode != null) {
+            if (slowNode.val != previousNode.val) {
+                return false;
+            }
+            slowNode = slowNode.next;
+            previousNode = previousNode.next;
+        }
+
+        return true;
+    }
+
+}
