18ef8753f16a6f66dd1f518cfb9eae8f0a58d6d3
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-18 18:52:52 -0300

    adding leetcode 206

---
+++ algoritmos/src/Leetcode/linkedList/ReverseLinkedList206/ReverseLinkedList.java
@@ -0,0 +1,26 @@
+package Leetcode.linkedList.ReverseLinkedList206;
+
+import Leetcode.linkedList.Util.ListNode;
+
+public class ReverseLinkedList {
+    static void main(String[] args) {
+        ListNode list1 = ListNode.createListNode(new int[]{2, 2, 1, 1, 2, 3, 3, 2});
+        ListNode.printList(list1);
+        System.out.println();
+        ListNode.printList(reverseList(list1));
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode previousNode = null;
+        ListNode currentNode = head;
+
+        while (currentNode != null) {
+            ListNode nextNodeAfterMoving = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNodeAfterMoving;
+        }
+
+        return previousNode;
+    }
+}
