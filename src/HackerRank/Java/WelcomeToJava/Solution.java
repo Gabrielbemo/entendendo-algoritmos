e4bf55514f8617763ae78854766303f3e575c2cc
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-24 20:18:34 -0300

    adding first welcome hackerrank challange

---
+++ src/HackerRank/Java/WelcomeToJava/Solution.java
@@ -0,0 +1,9 @@
+package HackerRank.Java.WelcomeToJava;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
