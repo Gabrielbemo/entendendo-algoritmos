2bdb7957d522b6baef2d1edaec8d60edc0c02263
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-25 18:43:20 -0300

    adding hackerrank stdin challange

---
+++ src/HackerRank/Java/JavaStdinAndStdoutI/Solution.java
@@ -0,0 +1,16 @@
+package HackerRank.Java.JavaStdinAndStdoutI;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a1 = scan.nextInt();
+        int a2 = scan.nextInt();
+        int a3 = scan.nextInt();
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+}
