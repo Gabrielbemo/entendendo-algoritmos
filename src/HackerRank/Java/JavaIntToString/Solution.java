6f9ba5db911eec387177221513325025cd47426d
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-31 19:02:44 -0300

    hackerrank JavaIntToString

---
+++ src/HackerRank/Java/JavaIntToString/Solution.java
@@ -0,0 +1,18 @@
+package HackerRank.Java.JavaIntToString;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        String s = Integer.toString(n);
+
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+    }
+}
