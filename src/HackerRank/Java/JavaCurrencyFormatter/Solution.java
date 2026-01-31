1213ffe79cec71c37fb0f22c3cf4620575ab6713
Gabrielbemo <gabrielbemoura@gmail.com>
2026-01-31 19:28:50 -0300

    hackerrank JavaCurrencyFormatter

---
+++ src/HackerRank/Java/JavaCurrencyFormatter/Solution.java
@@ -0,0 +1,38 @@
+package HackerRank.Java.JavaCurrencyFormatter;
+
+import java.io.*;
+import java.util.*;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = usFormat.format(payment);
+
+        Locale indiaLocale = new Locale("en", "IN");
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(indiaLocale);
+        DecimalFormatSymbols dfs = ((DecimalFormat) indiaFormat).getDecimalFormatSymbols();
+        dfs.setCurrencySymbol("Rs.");
+        ((DecimalFormat) indiaFormat).setDecimalFormatSymbols(dfs);
+        String india = indiaFormat.format(payment);
+
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = chinaFormat.format(payment);
+        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = franceFormat.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
