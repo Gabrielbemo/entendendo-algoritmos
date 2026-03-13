f115ac06efb6653acaae4e1391be9316f6758f56
Gabrielbemo <gabrielbemoura@gmail.com>
2026-03-13 10:27:23 -0300

    hackerrank ValidUsernameRegularExpression

---
+++ src/HackerRank/Java/ValidUsernameRegularExpression/Solution.java
@@ -0,0 +1,10 @@
+package HackerRank.Java.ValidUsernameRegularExpression;
+
+import java.util.Scanner;
+
+public class Solution {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[A-Za-z][A-Za-z0-9_]{7,29}$";
+}
