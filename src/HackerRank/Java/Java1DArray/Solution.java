3ab010d7b98d2ebaac0a2ed4b3dc7f6b3a4223dd
Gabrielbemo <gabrielbemoura@gmail.com>
2026-03-24 16:49:50 -0300

    hackrrank Java1DArray

---
+++ src/HackerRank/Java/Java1DArray/Solution.java
@@ -0,0 +1,25 @@
+package HackerRank.Java.Java1DArray;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
