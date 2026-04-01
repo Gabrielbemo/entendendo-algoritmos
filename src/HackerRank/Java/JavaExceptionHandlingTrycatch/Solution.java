e2308868881f5bc0edd0fc33f8c845f73a55d1cf
Gabrielbemo <gabrielbemoura@gmail.com>
2026-04-01 17:21:12 -0300

    hackerrank JavaExceptionHandlingTrycatch

---
+++ src/HackerRank/Java/JavaExceptionHandlingTrycatch/Solution.java
@@ -0,0 +1,20 @@
+package HackerRank.Java.JavaExceptionHandlingTrycatch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try{
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a/b);
+        }catch(InputMismatchException e){
+            System.out.println("java.util.InputMismatchException");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
