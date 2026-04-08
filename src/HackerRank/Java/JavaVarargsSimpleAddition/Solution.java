6c81ce9791de829070ef407c7ead7b5bad56fc86
Gabrielbemo <gabrielbemoura@gmail.com>
2026-04-08 16:20:29 -0300

    hackerrank JavaVarargsSimpleAddition

---
+++ src/HackerRank/Java/JavaVarargsSimpleAddition/Solution.java
@@ -0,0 +1,69 @@
+package HackerRank.Java.JavaVarargsSimpleAddition;
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Add {
+    public void add(int... numbers) {
+        int sum = 0;
+        StringBuilder expression = new StringBuilder();
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            expression.append(numbers[i]);
+
+            if (i < numbers.length - 1) {
+                expression.append("+");
+            }
+        }
+
+        expression.append("=").append(sum);
+        System.out.println(expression.toString());
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            int n1=Integer.parseInt(br.readLine());
+            int n2=Integer.parseInt(br.readLine());
+            int n3=Integer.parseInt(br.readLine());
+            int n4=Integer.parseInt(br.readLine());
+            int n5=Integer.parseInt(br.readLine());
+            int n6=Integer.parseInt(br.readLine());
+            Add ob=new Add();
+            ob.add(n1,n2);
+            ob.add(n1,n2,n3);
+            ob.add(n1,n2,n3,n4,n5);
+            ob.add(n1,n2,n3,n4,n5,n6);
+            Method[] methods=Add.class.getDeclaredMethods();
+            Set<String> set=new HashSet<>();
+            boolean overload=false;
+            for(int i=0;i<methods.length;i++)
+            {
+                if(set.contains(methods[i].getName()))
+                {
+                    overload=true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if(overload)
+            {
+                throw new Exception("Overloading not allowed");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
+
